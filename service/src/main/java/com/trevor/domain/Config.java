d6e66ae4e6af9afd732948b04a46b0a8feac2b65
trevorl <trevorl@synnex.com>
2019-03-07 12:05:05 +0800

    <fix>:update

--- service/src/main/java/com/trevor/domain/Config.java
+++ service/src/main/java/com/trevor/domain/Config.java
@@ -11,10 +11,79 @@ import lombok.Data;
 @Data
 public class Config {
 
+    /**
+     * 主键id
+     */
     private Long id;
 
+    /**
+     * 配置名字
+     */
     private String configName;
 
+    /**
+     * 配置的值
+     */
     private String configValue;
+
+    /**
+     * 配置描述
+     */
+    private String configDesc;
+
+    /**
+     * configValue 1
+     */
+    private String varchar1;
+
+    /**
+     * configValue 2
+     */
+    private String varchar2;
+
+    /**
+     * configValue 3
+     */
+    private String varchar3;
+
+    /**
+     * configValue 4
+     */
+    private String varchar4;
+
+    /**
+     * configValue 5
+     */
+    private String varchar5;
+
+    /**
+     * configValue 6
+     */
+    private String varchar6;
+
+    /**
+     * configValue 7
+     */
+    private String varchar7;
+
+    /**
+     * configValue 8
+     */
+    private String varchar8;
+
+    /**
+     * configValue 9
+     */
+    private String varchar9;
+
+    /**
+     * configValue 10
+     */
+    private String varchar10;
+
+    /**
+     * Y代表可用，N代表不可用
+     */
+    private String active;
 
 }

6ac54205708770fd66346ed9fc8ca214c5629c9c
trevorl <trevorl@synnex.com>
2019-03-08 19:46:51 +0800

    <fix>:update

--- web/src/main/java/com/trevor/web/controller/person/CardTransController.java
+++ web/src/main/java/com/trevor/web/controller/person/CardTransController.java
@@ -1,11 +1,14 @@
 package com.trevor.web.controller.person;
 
 import com.trevor.bo.JsonEntity;
+import com.trevor.service.cardTrans.CardTransService;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * @author trevor
@@ -14,8 +17,49 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CardTransController {
 
+    @Resource
+    private CardTransService cardTransService;
+
+    /**
+     * 创建房卡包
+     * @param cardNum
+     * @return
+     */
     @RequestMapping(value = "/api/cardTrans/create/package", method = {RequestMethod.POST}, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public JsonEntity<Object> createCardPackage(@RequestBody Integer cardNum ){
-       return null;
+    public JsonEntity<String> createCardPackage(@RequestBody Integer cardNum ){
+       return cardTransService.createCardPackage(cardNum ,null);
     }
+
+    /**
+     * 领取房卡包
+     * @param transNo
+     * @return
+     */
+    @RequestMapping(value = "/api/cardTrans/receive/package", method = {RequestMethod.POST}, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public JsonEntity<Object> createCardPackage(@RequestBody String transNo ){
+        return cardTransService.receiveCardPackage(transNo ,null);
+    }
+
+    /**
+     * 查询发出的房卡
+     * @param transNo
+     * @return
+     */
+    @RequestMapping(value = "/api/cardTrans/receive/package", method = {RequestMethod.POST}, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public JsonEntity<Object> findSendCardRecord(@RequestBody String transNo ){
+        return cardTransService.receiveCardPackage(transNo ,null);
+    }
+
+
+    /**
+     * 查询收到的房卡
+     * @param transNo
+     * @return
+     */
+    @RequestMapping(value = "/api/cardTrans/receive/package", method = {RequestMethod.POST}, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public JsonEntity<Object> findSenRecevedCardRecord(@RequestBody String transNo ){
+        return cardTransService.receiveCardPackage(transNo ,null);
+    }
+
+
 }

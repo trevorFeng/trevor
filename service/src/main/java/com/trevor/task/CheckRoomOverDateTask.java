e8f6d9a32c573dd1ac9849407b45b6d599abeaf8
trevorl <trevorl@synnex.com>
2019-05-20 17:42:50 +0800

    <fix>:update

--- service/src/main/java/com/trevor/task/CheckRoomOverDateTask.java
+++ service/src/main/java/com/trevor/task/CheckRoomOverDateTask.java
@@ -6,7 +6,6 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
-import java.util.concurrent.Executor;
 
 /**
  * @author trevor
@@ -24,7 +23,7 @@ public class CheckRoomOverDateTask{
         log.info("检查房间开始");
         //房间半小时内未使用会被关闭
         try {
-            //taskService.checkRoomRecord();
+            taskService.checkRoomRecord();
         }catch (Exception e) {
             e.printStackTrace();
             log.error(e.toString());
